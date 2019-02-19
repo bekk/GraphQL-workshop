308de783a26a201ee9b68934f19079af471c7453
Ketil Velle <ketil.velle@bekk.no>
2019-02-19 20:40:43 +0100

    Lagt til typedefs for futurama.json og koblet det hele sammen.

--- GraphqlServer.js
+++ GraphqlServer.js
@@ -1,23 +1,13 @@
 const fs = require('fs');
-
 const { ApolloServer, gql } = require('apollo-server');
-
-
-// The GraphQL schema
-const typeDefs = gql`
-  type Query {
-    "A simple type for getting started!"
-    hello: String
-  }
-`;
+const typeDefs = require('./typedef')
 
 const data = JSON.parse(fs.readFileSync('futurama.json', 'UTF-8'));
-
 
 // A map of functions which return data for the schema.
 const resolvers = {
     Query: {
-        hello: () => 'world'
+        futurama: () => data
     }
 };
 
@@ -26,8 +16,6 @@ const server = new ApolloServer({
     resolvers,
 });
 
-
 server.listen().then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`)
-    console.log(data._embedded.episodes[0].name);
 });
--- typedef.js
+++ typedef.js
@@ -1,3 +1,40 @@
-const typedef = `
+const { gql } = require('apollo-server');
 
+const typeDefs = gql`
+    type Query {
+        futurama: Futurama
+    }
+
+    type Futurama {
+        id: Int
+        url: String
+        name: String
+        type: String
+        language: String
+        genres: [String]
+        status: String
+        premiered: String
+        officialSite: String
+        image: Image
+        summary: String
+        episodes: [Episode]
+     }
+     
+     type Image {
+        medium: String
+        original: String
+     }
+
+     type Episode {
+        id: Int
+        url: String
+        name: String
+        season: Int
+        number: Int
+        image: Image
+        summary: String
+     }
 `
+
+
+module.exports = typeDefs;
