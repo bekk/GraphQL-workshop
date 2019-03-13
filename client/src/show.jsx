c23565ccfa75278e61faa7799f9b1591bec45010
Kjell Arne Brødreskift <kjarbr@gmail.com>
2019-03-13 11:44:29 +0100

    Visning av show.

--- client/src/show.jsx
+++ client/src/show.jsx
@@ -28,34 +28,37 @@ class Show extends React.Component {
                     const distinctSeasons = [...new Set(data.show.episodes.map(episode => episode.season))];
 
                     return (
-                        <div key={data.show.id} className="show">
-                            <h1>
-                                {data.show.name}{' '}
-                                <span className="aarstall">({data.show.premiered.split('-')[0]})</span>
-                            </h1>
-                            <div className="subtext">
-                                <span>{data.show.type}</span>
-                                {' | '}
-                                <span>{data.show.status}</span>
-                                {' | '}
-                                <span>{data.show.genres.join(' | ')}</span>
-                                {' | '}
-                                <span className="lenke">
+                        <>
+                            <div key={data.show.id} className="show">
+                                <h1>
+                                    {data.show.name}{' '}
+                                    <span className="aarstall">({data.show.premiered.split('-')[0]})</span>
+                                </h1>
+                                <div className="content">
+                                    <img src={data.show.image.medium} alt=""/>
+                                    <div>
+                                        <div className="subtext">
+                                            <span>{data.show.type}</span>
+                                            {' | '}
+                                            <span>{data.show.status}</span>
+                                            {' | '}
+                                            <span>{data.show.genres.join(' | ')}</span>
+                                            {' | '}
+                                            <span className="lenke">
                                     <a href={data.show.officialSite} target="_blank" rel="noopener noreferrer">{data.show.officialSite}</a>
                                 </span>
-                            </div>
-                            <div className="content">
-                                <img src={data.show.image.medium} alt=""/>
-                                <div>
-                                    <div className="summary">{Parser(data.show.summary)}</div>
-                                    <div className="seasons">
-                                        {distinctSeasons.map(season => {
-                                            return <a href="#" onClick={showSeasons} key={season}>Season {season}</a>
-                                        })}
+                                        </div>
+                                        <div className="summary">{Parser(data.show.summary)}</div>
+                                        <div className="seasons">
+                                            {distinctSeasons.map(season => {
+                                                return <a href="#" onClick={showSeasons} key={season}>Season {season}</a>
+                                            })}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                            <button onClick={() => window.history.back()}>Back</button>
+                        </>
                     );
                 }}
             </Query>
