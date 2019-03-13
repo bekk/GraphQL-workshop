c18bd17c2a3c02511c62b499b05532def04b79f0
Ketil Velle <ketil.velle@bekk.no>
2019-03-13 21:53:37 +0100

    Fjernet ubrukt query

--- server/graphqlServer.js
+++ server/graphqlServer.js
@@ -18,11 +18,6 @@ const resolvers = {
         },
         shows: () => data.shows,
     },
-    Show: {
-        episode(obj, args, context, info) {
-            return findEpisode(args.id);
-        }
-    },
     Mutation: {
         createComment(obj, args, context, info) {
             let show = findShow(args.showId);
