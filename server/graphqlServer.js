61721abcc916fd9d2d7da0754f9b9bd9e6b65ec3
Kjell Arne Brødreskift <kjarbr@gmail.com>
2019-03-15 12:56:16 +0100

    Løsning for oppgave 3

--- server/graphqlServer.js
+++ server/graphqlServer.js
@@ -18,13 +18,6 @@ const resolvers = {
         },
         shows: () => data.shows,
     },
-    Mutation: {
-        createComment(obj, args, context, info) {
-            let show = findShow(args.showId);
-            show.comments ? show.comments.push(args.comment) : show.comments = [args.comment];
-            return show;
-        }
-    }
 };
 
 function findShow(id) {
--- server/typedef.js
+++ server/typedef.js
@@ -6,42 +6,10 @@ const typeDefs = gql`
         shows: [Show]
     }
 
-    type Mutation {
-        createComment(showId: Int ,comment: String): Show
-    }
-
-    type Show {
-        id: Int
-        url: String
-        name: String
-        type: String
-        language: String
-        genres: [String]
-        status: String
-        premiered: String
-        officialSite: String
-        image: Image
-        summary: String
-        episodes: [Episode]
-        comments: [String]
-    }
-
      type Image {
         medium: String
         original: String
     }
-
-    type Episode {
-        id: Int
-        url: String
-        name: String
-        season: Int
-        airdate: String
-        number: Int
-        image: Image
-        summary: String
-    }
 `;
-
 
 module.exports = typeDefs;
