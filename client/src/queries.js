e21d7f151e199883810e353281521db560191d91
Kjell Arne Brødreskift <kjarbr@gmail.com>
2019-03-15 12:04:02 +0100

    Løsning for oppgavestart

--- client/src/queries.js
+++ client/src/queries.js
@@ -4,40 +4,5 @@ export const GET_SHOWS = gql`{
     shows {
         id,
         name,
-        type,
-        status,
-        premiered,
-        officialSite,
-        image {
-            medium
-        },
     }
 }`;
-
-export const GET_SHOW = gql`
-    query GetShow($idNumber: Int!) {
-        show(id: $idNumber) {
-            id,
-            name,
-            summary,
-            type,
-            genres,
-            status,
-            premiered,
-            officialSite,
-            comments,
-            image {
-                medium
-            },
-            episodes {
-                name,
-                summary,
-                season,
-                airdate,
-                image {
-                    medium
-                }
-            }
-        }
-    }
-`;
