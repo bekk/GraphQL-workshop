edbf8eeef89ec8e3f3909d7568c9c4d2c54fde53
Kjell Arne Brødreskift <kjarbr@gmail.com>
2019-03-15 13:01:52 +0100

    Løsning for oppgavestart

--- server/typedef.js
+++ server/typedef.js
@@ -2,7 +2,13 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
     type Query {
+        show(id: Int): Show
         shows: [Show]
+    }
+
+    type Show {
+        id: Int
+        name: String
     }
 `;
 
