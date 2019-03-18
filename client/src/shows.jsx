3dd1d57be1b9e709169c12d794a62f4abb7624b4
Kjell Arne Brødreskift <kjarbr@gmail.com>
2019-03-18 12:35:35 +0100

    Fjernet stort bilde i listen

--- client/src/shows.jsx
+++ client/src/shows.jsx
@@ -7,23 +7,6 @@ import './shows.css';
 
 class Shows extends React.Component {
 
-    handleClickLine = (e) => {
-        this.handleClickFull();
-        const full = e.target.parentElement.querySelector('.full');
-        if (full) {
-            full.classList.add('view');
-            full.style.top = e.target.getBoundingClientRect().top + 'px';
-            full.style.left = e.target.getBoundingClientRect().left + 'px';
-        }
-    };
-
-    handleClickFull = () => {
-        const expanded = document.querySelectorAll('img.full');
-        expanded.forEach(img => {
-            img.classList.remove('view');
-        });
-    };
-
     handleClickRow = (e, id) => {
         if (!e.target.classList.contains('noclick') && !e.target.parentElement.classList.contains('noclick')) {
             this.props.history.push('/show/' + id);
@@ -40,11 +23,7 @@ class Shows extends React.Component {
                         </tr>
                     </thead>
                     <tbody>
-                        <ShowList
-                            handleClickRow={this.handleClickRow}
-                            handleClickLine={this.handleClickLine}
-                            handleClickFull={this.handleClickFull}
-                        />
+                        <ShowList handleClickRow={this.handleClickRow} />
                     </tbody>
                 </table>
             </>
